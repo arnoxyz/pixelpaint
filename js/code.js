ac5c887f324946e83fb6e19dcdde2cab2d30d08b
Arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2023-04-11 14:59:11 +0200

    creat pixel grid

--- js/code.js
+++ js/code.js
@@ -1,7 +1,18 @@
 
 
+const pixelGridField = document.querySelector("#pixel-grid");
 
+for(let i = 1; i <= 100; i++){
+	createPixelDiv(i);
+}
 
-const pixelGridField = document.querySelector(".pixel-grid");
-pixelGridField.innerHTML = "hello World!";
+function createPixelDiv(id){	
+	const div = document.createElement("div");
+	div.innerText= id;
+	div.style.width = "100px";
+	div.style.height= "100px";
+	div.style.background= "white";
+	pixelGridField.appendChild(div);
+}
+
 
