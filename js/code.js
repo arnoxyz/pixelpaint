2ba12ab1b6b981380d7c5a8cbd019f074fc01462
Arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2023-04-11 16:14:29 +0200

    add mouseover, coloring pixel grid

--- js/code.js
+++ js/code.js
@@ -1,18 +1,42 @@
+main();
 
+
+/*
+ * main function all 
+*/
+function main(){
+	createPixelGrid(100);
+}
 
 const pixelGridField = document.querySelector("#pixel-grid");
 
-for(let i = 1; i <= 100; i++){
-	createPixelDiv(i);
+/*
+ * Create the pixel grid
+*/
+function createPixelGrid(size)
+{
+	for(let i = 1; i <= size; i++){
+		createPixelDiv(i);
+	}
 }
 
+
+/*
+ * creates a div 
+ */
 function createPixelDiv(id){	
+	const pixelGridField = document.querySelector("#pixel-grid");
 	const div = document.createElement("div");
-	div.innerText= id;
+	//div.innerText= id;
+	div.id = id;
 	div.style.width = "100px";
 	div.style.height= "100px";
-	div.style.background= "white";
+	div.style.background= "red";
 	pixelGridField.appendChild(div);
+
+	div.addEventListener('mouseover', () => {
+		div.style.background = "white";
+	});
 }
 
 
